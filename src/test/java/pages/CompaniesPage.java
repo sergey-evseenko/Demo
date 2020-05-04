cc65bccf9322c20b0c1a362c31cde6477ee3e41e
Sergey Evseenko <evseenkosv@tut.by>
2020-05-04 19:25:10 +0300

    Added CompaniesPage.java

--- src/test/java/pages/CompaniesPage.java
+++ src/test/java/pages/CompaniesPage.java
@@ -1,6 +1,8 @@
 package pages;
 
 import io.qameta.allure.Step;
+import models.Company;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -13,13 +15,17 @@ public class CompaniesPage extends BasePage {
     @FindBy(css = ".btn-primary")
     WebElement createCompanyButton;
 
+    @FindBy(css = "input[type='search']")
+    WebElement searchInput;
+
+
     public CompaniesPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
         PageFactory.initElements(driver, this);
     }
 
-    @Override
     public BasePage isPageOpened() {
+        wait.until(ExpectedConditions.visibilityOf(createCompanyButton));
         return null;
     }
 
@@ -30,9 +36,24 @@ public class CompaniesPage extends BasePage {
 
     @Step("Clicking Create company button")
     public CreateCompanyPage createCompanyButtonClick() {
-        wait.until(ExpectedConditions.visibilityOf(createCompanyButton));
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.navigate().refresh();
+        isPageOpened();
         createCompanyButton.click();
         return new CreateCompanyPage(driver, wait);
 
+    }
+
+    @Step("Searching and opening company")
+    public CompanyDetailsPage searchAndOpenCompany(Company company) {
+        String locator = "//div[contains(text(),%s)]";
+        isPageOpened();
+        searchInput.sendKeys(company.getCompanyName());
+        driver.findElement(By.xpath(String.format(locator, company.getCompanyName()))).click();
+        return new CompanyDetailsPage(driver, wait);
     }
 }
